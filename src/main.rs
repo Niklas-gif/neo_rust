231218b089e8a4d8ebd2c36b91b810435172c5fb
Niklas D <80592781+Niklas-gif@users.noreply.github.com>
2026-02-28 12:43:58 +0100

    palceholder art

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,32 @@ use ratatui::{
 
 use std::{fs, io};
 
+const PLACE_HOLDER: &str = "
+┌──────────────────────────────────────────────────┐
+│                                                  │
+│           xx                                     │
+│          xx     xx    xxxxx       x xxxx         │
+│        xx x    xx    x    xxx     xxx  xxx       │
+│       xx  x  xx      x      xx    x      xx      │
+│       x  xx xx      xxxx         xx       x      │
+│      x   x xx      xx   xxxx     x        xx     │
+│     x    xx        x             x         x     │
+│   xxx    xx       x     xx       x        x      │
+│  xx      x        xxxxxxxxx      xxx    xx       │
+│                                    xxxxx         │
+│          xxx                                     │
+│         xx x xx    x        xxxx     xxx         │
+│        x      x   x     x  x   x       xxxx      │
+│       xx      x  x      x  x          xx  xxxx   │
+│       x xxxxxx   x     xx   xxxx      x          │
+│      xx   xx     x     x        xx    x          │
+│      x      xxx  xx  xx      x xxx    xx         │
+│               x   xxxx        xx      x          │
+│                                     xx           │
+│                                     x            │
+│                                                  │
+└──────────────────────────────────────────────────┘";
+
 //Get OS --> /etc/os-release --> pretty name
 //Get CPU --> /proc/cpuinfo --> model name
 //Get GPU --> lspci --> 09:00.0 VGA compatible controller: Advanced Micro Devices, Inc. [AMD/ATI] Navi 22 [Radeon RX 6700/6700 XT / 6800M] (rev c1)
@@ -145,7 +171,7 @@ impl Widget for &App {
             .title(title.centered())
             .title_bottom(instructions.centered())
             .border_set(border::THICK);
-        //TODO Remove clones
+
         let sys_text = Text::from(vec![Line::from(vec![
             "OS: ".into(),
             self.sys_info.os.as_str().red(),
