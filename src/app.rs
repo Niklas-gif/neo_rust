8de0d3d86e07fee624284cf536888e3ea2980575
Niklas D <80592781+Niklas-gif@users.noreply.github.com>
2026-03-01 10:23:05 +0100

    separate app and UI

--- src/app.rs
+++ src/app.rs
@@ -16,6 +16,11 @@ use ratatui::{
 
 use std::io;
 
+pub enum AppState {
+    Main,
+    Exiting,
+}
+
 #[derive(Debug)]
 pub struct App {
     pub sys_info: sys_info::SysInfo,
@@ -23,7 +28,7 @@ pub struct App {
 }
 
 impl App {
-    pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
+    /*pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
         while !self.exit {
             terminal.draw(|frame| self.draw(frame))?;
             self.handle_events()?;
@@ -33,13 +38,13 @@ impl App {
 
     fn draw(&self, frame: &mut Frame) {
         frame.render_widget(self, frame.area());
-    }
+    }*/
 
-    fn exit(&mut self) {
+    pub fn exit(&mut self) {
         self.exit = true;
     }
 
-    fn handle_key_event(&mut self, key_event: KeyEvent) {
+    /*fn handle_key_event(&mut self, key_event: KeyEvent) {
         match key_event.code {
             KeyCode::Char('q') => self.exit(),
             _ => {}
@@ -54,9 +59,9 @@ impl App {
             _ => {}
         };
         Ok(())
-    }
+    }*/
 
-    fn demo_widget(&self, area: Rect, buf: &mut Buffer) {
+    /*fn demo_widget(&self, area: Rect, buf: &mut Buffer) {
         let title = Line::from("Neo Rust".bold());
         let instructions = Line::from(vec![" Quit ".into(), "<Q> ".red().bold()]);
         let block = Block::bordered()
@@ -74,11 +79,11 @@ impl App {
             .right_aligned()
             .block(block)
             .render(area, buf);
-    }
+    }*/
 }
 
-impl Widget for &App {
+/*impl Widget for &App {
     fn render(self, area: Rect, buf: &mut Buffer) {
         self.demo_widget(area, buf);
     }
-}
+}*/
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,53 @@
 mod app;
 mod sys_info;
 mod ui;
+use crate::app::App;
+use crate::ui::ui;
 
-use std::io;
+use std::{error::Error, io};
 
-fn main() -> Result<(), io::Error> {
+use ratatui::{
+    Terminal,
+    backend::{Backend, CrosstermBackend},
+    crossterm::{
+        event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode, KeyEventKind},
+        execute,
+        terminal::{EnterAlternateScreen, LeaveAlternateScreen, disable_raw_mode, enable_raw_mode},
+    },
+};
+
+fn main() -> Result<(), Box<dyn Error>> {
     let linux = sys_info::LinuxInfo::default();
 
     let mut app = app::App {
         sys_info: linux.sys_info,
         exit: false,
     };
-    ratatui::run(|terminal| app.run(terminal))
+
+    enable_raw_mode()?;
+    let mut stderr = io::stderr(); // This is a special case. Normally using stdout is fine
+    execute!(stderr, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stderr);
+    let mut terminal = Terminal::new(backend)?;
+
+    run(&mut terminal, &mut app);
+    Ok(())
+}
+
+fn run<B: Backend>(terminal: &mut Terminal<B>, app: &mut App) -> io::Result<bool> {
+    while !app.exit {
+        terminal.draw(|frame| ui(frame, app));
+        if let Event::Key(key) = event::read()? {
+            if key.kind == event::KeyEventKind::Release {
+                // Skip events that are not KeyEventKind::Press
+                continue;
+            }
+            //self.handle_events()?;
+            match key.code {
+                KeyCode::Char('q') => app.exit(),
+                _ => {}
+            }
+        }
+    }
+    return Ok(true);
 }
